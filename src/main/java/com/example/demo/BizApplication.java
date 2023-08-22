b9b173a0258dfac7d3e5f7722d6ddd51b3de9d5d
wwl <why1234me@163.com>
2023-08-22 15:54:10 +0800

    refactor biz project

--- src/main/java/com/example/demo/BizApplication.java
+++ src/main/java/com/example/demo/BizApplication.java
@@ -3,10 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-public class LoginApplication {
+public class BizApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
+		SpringApplication.run(BizApplication.class, args);
 	}
 
 }
